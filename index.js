4eed81c85b0f59fa4782c253e634d26f1f9bc6cc
franciscompereira202212-hue <franciscompereira202212@gmail.com>
2026-04-28 09:36:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ if (!fs.existsSync("./uploads")) {
   fs.mkdirSync("./uploads");
 }
 
+// Upload
 app.post("/upload", async (req, res) => {
   if (!req.files || !req.files.demo) {
     return res.send("No file uploaded");
   }
 
   const file = req.files.demo;
-
   await file.mv("./uploads/" + file.name);
 
   res.send("Upload feito!");
+});
+
+// 🔥 NOVO: listar demos
+app.get("/demos", (req, res) => {
+  const files = fs.readdirSync("./uploads");
+  res.json(files);
 });
 
 app.listen(3000, () => console.log("Servidor ligado"));
