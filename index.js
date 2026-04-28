67d2be47b08713397a05871f8a423ea5c95c7ead
franciscompereira202212-hue <franciscompereira202212@gmail.com>
2026-04-28 09:27:01 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+
+const app = express();
+app.use(fileUpload());
+app.use(express.static("public"));
+
+if (!fs.existsSync("./uploads")) {
+  fs.mkdirSync("./uploads");
+}
+
+app.post("/upload", async (req, res) => {
+  if (!req.files || !req.files.demo) {
+    return res.send("No file uploaded");
+  }
+
+  const file = req.files.demo;
+
+  await file.mv("./uploads/" + file.name);
+
+  res.send("Upload feito!");
+});
+
+app.listen(3000, () => console.log("Servidor ligado"));
